0ef47dd1bbb383ce8b2c1375d3699e43bd0cd1d2
l-lang <dummy@blafasel.de>
2019-11-06 22:06:51 +0100

    Fixed wrong resistor (from 22k to 200k in oscilator microphone) Inital sketch

--- code/BassKatze/BassKatze.ino
+++ code/BassKatze/BassKatze.ino
@@ -1,24 +1,48 @@
-#define RAW_SPECTRUM_AIN PC0
-#define GAIN_SELECT_OUT PC1
-#define ATTACK_SELECT_OUT PC2
-#define EQ_BAND_OUT PD0
-#define EQ_RESET_OUT PD1
-#define LED1_OUT PD2
-#define BUTTON_IN PD4
-#define PWR_HOLD_OUT PD5
-#define CHRG_IN PD6
-#define STDBY_IN PD7
+#define RAW_SPECTRUM_AIN A0 // PC0
+#define GAIN_SELECT_OUT A1 // PC1
+#define ATTACK_SELECT_OUT A2 // PC2
+#define EQ_BAND_OUT A4 // PC4
+#define EQ_RESET_OUT A5 // PC5
+#define LED1_OUT 9 // PB1
+#define BUTTON_IN 4 // PD4
+#define PWR_HOLD_OUT 5 // PD5
+#define CHRG_IN 6 // PD6
+#define STDBY_IN 7 // PD7
+
+int spectrumValue[7];
+int filter=80;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   analogReference(INTERNAL);
-  pinMode(13, OUTPUT);
+  
+  Serial.begin(9600);
+  pinMode(RAW_SPECTRUM_AIN, INPUT);
+  pinMode(EQ_BAND_OUT, OUTPUT);
+  pinMode(EQ_RESET_OUT, OUTPUT);
+  pinMode(LED1_OUT, OUTPUT);
+  
+  digitalWrite(EQ_RESET_OUT, LOW);
+  digitalWrite(EQ_BAND_OUT, HIGH);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
+void loop(){
+
+  digitalWrite(EQ_RESET_OUT, HIGH);
+  digitalWrite(EQ_RESET_OUT, LOW);
+  for (int i=0;i<7;i++){
+    digitalWrite(EQ_BAND_OUT, LOW);
+    delay(10);
+    spectrumValue[i]=analogRead(RAW_SPECTRUM_AIN);
+    spectrumValue[i]=constrain(spectrumValue[i], filter, 1023);
+    spectrumValue[i]=map(spectrumValue[i], filter,1023,0,255);
+    digitalWrite(EQ_BAND_OUT, HIGH);
+  }
+  Serial.println();
+
+  analogWrite(LED1_OUT,spectrumValue[0]);
+//  delay(100);
+//  analogWrite(LED1_OUT, LOW);
+//  delay(100);
 }
